0037aa7a30564a4ab5a43565dbaa9f13f977dd66
Melvin Junhee Woo <melvin.woo@groundx.xyz>
2020-05-28 17:45:24 +0900

    triesInMemory to 64

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -69,7 +69,7 @@ const (
 )
 
 const (
-	triesInMemory = 4
+	triesInMemory = 64
 	// BlockChainVersion ensures that an incompatible database forces a resync from scratch.
 	BlockChainVersion    = 3
 	DefaultBlockInterval = 128
