f7e41994c57d9812bec269f912499b3404475439
Melvin Junhee Woo <melvin.woo@groundx.xyz>
2020-09-11 10:56:40 +0900

    numcpu

--- storage/statedb/database.go
+++ storage/statedb/database.go
@@ -1146,5 +1146,5 @@ func (db *Database) SaveTrieNodeCacheToFile() error {
 	if db.trieNodeCache == nil {
 		return nil
 	}
-	return db.trieNodeCache.SaveToFile(db.diskDB.GetDBConfig().Dir, runtime.NumCPU()/2)
+	return db.trieNodeCache.SaveToFile(db.diskDB.GetDBConfig().Dir, runtime.NumCPU())
 }
