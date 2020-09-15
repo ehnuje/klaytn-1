c3de4e4ac87ecbc92b4dfe59be89ea90ab8c5562
Melvin Junhee Woo <melvin.woo@groundx.xyz>
2020-09-15 17:03:30 +0900

    timeout 1ms to 1m

--- fastcache/file.go
+++ fastcache/file.go
@@ -38,7 +38,7 @@ func (c *Cache) SaveToFile(filePath string) error {
 //
 // See also SaveToFile.
 func (c *Cache) SaveToFileConcurrent(filePath string, concurrency int) error {
-	ctx, _ := context.WithTimeout(context.Background(), 1*time.Millisecond)
+	ctx, _ := context.WithTimeout(context.Background(), 1*time.Minute)
 	return c.SaveToFileConcurrentWithContext(filePath, concurrency, ctx)
 	// Create dir if it doesn't exist.
 	//dir := filepath.Dir(filePath)
